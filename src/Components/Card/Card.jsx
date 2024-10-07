87bb2f00fb302b9c36feea09383e4d36f89385a2
Amar-03creator <fiku456@gmail.com>
2024-10-07 12:35:00 +0530

    Game Ends After Winner Declaration

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import Icon from "../Icon/icon"
 import "./Card.css"
-function Card({ player , onPlay , index }){
+function Card({gameEnd ,  player , onPlay , index }){
     let icon = <Icon/>
     if(player == 'X'){
         icon = <Icon name = "cross" />
@@ -8,7 +8,7 @@ function Card({ player , onPlay , index }){
         icon = <Icon name = "circle" />
     }
     return (
-        <div className="card" onClick={() => onPlay(index)}>
+        <div className="card" onClick={() => !gameEnd && onPlay(index)}>
             {icon}
         </div>
     )
